f18c557fe921008b29e5352a2c662afae0ad8f3c
Ahana Das <ahanadas@Ahanas-MacBook.local>
2026-01-12 13:13:12 +0530

    Day4: TypeScript basic types

---
+++ 01-types.ts
@@ -0,0 +1,22 @@
+// number type
+let age: number = 21;
+
+// string type
+let name1: string = "Ahana";
+
+// boolean type
+let isStudent: boolean = true;
+
+// array of numbers
+let marks: number[] = [80, 85, 90];
+
+// tuple (fixed order + types)
+let user: [string, number] = ["Ahana", 21];
+
+// object with type
+let profile: { name: string; age: number } = {
+    name: "Ahana",
+    age: 21
+};
+
+console.log(age, name1, isStudent, marks, user, profile);
