5fec24f2b33d809eb9b8facd1a25ffb9b80820ac
Ahana <ahana.das@msds.christuniversity.in>
2026-01-13 11:11:50 +0530

    Delete TypeScript Plan directory

--- TypeScript Plan/day3/defaultparameter.ts
+++
@@ -1,6 +0,0 @@
-function getScoreWithDefault(score: number, bonus: number = 5): number {
-    return score + bonus;
-}
-
-console.log(getScoreWithDefault(80));
-console.log(getScoreWithDefault(80, 10)); 
--- TypeScript Plan/day3/functions.js
+++
@@ -1,5 +0,0 @@
-function calculateFinalScore(testScore, bonus) {
-    return testScore + bonus;
-}
-var result = calculateFinalScore(80, 5);
-console.log(result);
